592679b93f43d1ac3e45ada3fb540735413f1233
Marcial Sánchez <marcialj.sanchez@gmail.com>
2015-11-10 17:46:08 +0100

    [NEW] Se ha sacado la clase ForeCastFragment de Main. Añadida conexion http en un nuevo hilo.

--- app/src/main/java/com/example/blitzer/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/blitzer/sunshine/app/MainActivity.java
@@ -1,17 +1,7 @@
 package com.example.blitzer.sunshine.app;
 
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,43 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
 
-    public static class PlaceholderFragment extends Fragment {
-
-        ArrayAdapter<String> listAdapter;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-             String[] datos = {
-                "Hoy - Soleado - 20/25",
-                "Mañana - Nuboso - 15/20",
-                "Viernes - Lluvia - 10/15",
-                "Sábado - Soleado - 22/25",
-                "Domingo - Tormenta - 11/16",
-                "Lunes - Soleado - 15/20",
-            };
-
-            List<String> forecastEntries = new ArrayList<String>(Arrays.asList(datos));
-
-            listAdapter = new ArrayAdapter<String>(
-                    getActivity(),
-                    R.layout.list_item_forecast,
-                    R.id.list_item_forecast_textview,
-                    forecastEntries);
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(listAdapter);
-
-            return rootView;
-        }
-    }
 }
